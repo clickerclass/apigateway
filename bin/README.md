426d0909e3c3c50dc32931658fb177a746ed2adc
Manuel alejandro alcala <alejoab12@hotmail.com>
2020-08-20 13:43:38 -0500

    original sources

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+# toures-balon-api-gateway
---
+++ src/main/java/com/clickerclass/apigateway/Application.java
@@ -0,0 +1,37 @@
+package com.toures.balon;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = { "com.clickerclass", "com.gema.autentication" })
+
+public class Application {
+
+	@Autowired
+	private GatewayProperties props;
+
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		logger.warn(Objects.toString(props));
+	}
+
+}
